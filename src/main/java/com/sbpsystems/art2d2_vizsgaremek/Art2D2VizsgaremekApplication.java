3b6b7fbf5fafb44612b1a88b1836ce328a611f23
Tibor Beres <beres.tibor@sbpsystems.hu>
2024-10-29 11:54:52 +0100

    init

---
+++ src/main/java/com/sbpsystems/art2d2_vizsgaremek/Art2D2VizsgaremekApplication.java
@@ -0,0 +1,13 @@
+package com.sbpsystems.art2d2_vizsgaremek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Art2D2VizsgaremekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Art2D2VizsgaremekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbpsystems/art2d2_vizsgaremek/Art2D2VizsgaremekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbpsystems.art2d2_vizsgaremek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Art2D2VizsgaremekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
